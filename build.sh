a28a06baa7b9b3e6e2643daad7400d4d4a712db8
Maximilian Matthe <maxi.matthe@googlemail.com>
2021-03-16 14:17:08 +0100

    bump release to 20201211

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ORG=barkhauseninstitut
 IMAGE=ros2custom
-VERSION=foxy
+VERSION=foxy20201211
 TAG_BUILD=$ORG/$IMAGE:$VERSION
 
 docker build -t $TAG_BUILD -f Dockerfile.foxy --build-arg CACHEBUST=$(date +%s) .
